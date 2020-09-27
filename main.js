575d57614dbdf6e8ebd47ef7295cadb68c0096fc
Kasra Kayvani <kasrakayvani@gmail.com>
2020-09-27 22:40:38 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+let getData = ( ) =>
+{ 
+let city = document.querySelector("#cityName").value;
+let appId = '--PLACEHOLDER FOR YOUR API KEY--';
+apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&${appId}`;
+
+fetch(apiUrl)
+    .then ((resp) => resp.json())
+    .then(apiResponse =>  {
+
+if ( city === '' ) { 
+const errorElement = document.createElement('h2');
+errorElement.innerHTML = 'Please enter a valid city name';
+document.querySelector('#output').appendChild(errorElement);
+} 
+else { 
+const newElement = document.createElement('h3');
+newElement.innerHTML = 
+` ${city}:  ${Math.round(apiResponse.main.temp - 273.15)} &deg;`;
+document.querySelector('#output').appendChild(newElement);
+} 
+} );
+}
+document.querySelector('#citySearch').addEventListener('click', getData);
